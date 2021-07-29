5ef27c161b1704cb66485301b8ad691e645effa3
thewolf27 <wolf-front@yandex.ru>
2021-07-29 06:54:05 +0300

    Second Homework Done (With *)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,23 +11,40 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	var (
-		newstr strings.Builder
-		letter string
-		append string
+		newstr    strings.Builder
+		letter    string
+		append    string
+		convert   bool
+		echoSlash bool
 	)
 	for _, sym := range str {
-		if unicode.IsDigit(sym) {
+		if unicode.IsDigit(sym) && !convert {
 			if letter == "" {
 				return "", ErrInvalidString
 			}
-
 			count, _ := strconv.Atoi(string(sym))
-
 			append = strings.Repeat(letter, count)
 			letter = ""
 		} else {
 			append = letter
+			if sym == '\\' {
+				if letter == "\\" && !echoSlash {
+					append = ""
+					echoSlash = true
+				} else {
+					echoSlash = false
+				}
+			} else {
+				if letter == "\\" {
+					append = ""
+				}
+			}
 			letter = string(sym)
+
+			convert = false
+			if letter == "\\" && !echoSlash {
+				convert = true
+			}
 		}
 		newstr.WriteString(append)
 	}
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -7,22 +7,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type tests struct {
+	input    string
+	expected string
+}
+
 func TestUnpack(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected string
-	}{
+	tests := []tests{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
-
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
@@ -40,6 +41,29 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackMyCases(t *testing.T) {
+	tests := []tests{
+		{input: `f\\\\\\\\\\\\\\1`, expected: `f\\\\\\\`},
+		{input: `a2da\\\9`, expected: `aada\9`},
+		{input: `a9b9c9\9\9ss`, expected: `aaaaaaaaabbbbbbbbbccccccccc99ss`},
+		{input: `\\5\\\3\j\\\\`, expected: `\\\\\\3j\\`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "d\n5\n\n\n\n\n\nnnnn\n6abc", expected: "d\n\n\n\n\n\n\n\n\n\n\nnnnn\n\n\n\n\n\nabc"},
+	}
+	runTests(t, tests)
+}
+
+func runTests(t *testing.T, tests []tests) {
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
