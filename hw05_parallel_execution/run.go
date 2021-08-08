b343aee5c7951e053ab452855b94173908046fb9
thewolf27 <wolf-front@yandex.ru>
2021-08-08 09:25:34 +0300

    Hw05 Done (With *)

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,52 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
+type TaskCh chan Task
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func runTask(taskCh TaskCh, wg *sync.WaitGroup, maxErrCount *int64) {
+	for task := range taskCh {
+		if atomic.LoadInt64(maxErrCount) > 0 {
+			err := task()
+			if err != nil {
+				atomic.AddInt64(maxErrCount, -1)
+			}
+		}
+		wg.Done()
+	}
+}
+
+func addTasksToTaskCh(taskCh TaskCh, tasks []Task) {
+	for _, task := range tasks {
+		taskCh <- task
+	}
+	close(taskCh)
+}
+
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	taskCh := make(TaskCh, n)
+
+	var wg sync.WaitGroup
+	wg.Add(len(tasks))
+
+	var maxErrCount int64
+	atomic.StoreInt64(&maxErrCount, int64(m))
+
+	for i := 0; i < n; i++ {
+		go runTask(taskCh, &wg, &maxErrCount)
+	}
+
+	go addTasksToTaskCh(taskCh, tasks)
+
+	wg.Wait()
+	if maxErrCount < 1 {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
