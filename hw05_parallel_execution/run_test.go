ab17a1884ff5f57237d4bb5029d0e328593f4852
Artur <60509074+thewolf27@users.noreply.github.com>
2021-08-08 09:28:09 +0300

    Hw05 Done (With *)

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -68,3 +68,90 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 }
+
+func TestMyCases(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	t.Run("zero maxErrorsCount", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.Equal(t, int32(0), runTasksCount)
+	})
+
+	t.Run("zero tasks", func(t *testing.T) {
+		tasksCount := 0
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		workersCount := 10
+		maxErrorsCount := 10
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Nil(t, err)
+		require.Equal(t, int32(0), runTasksCount)
+	})
+
+	t.Run("a lot of tasks", func(t *testing.T) {
+		tasksCount := 100000
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 100
+		maxErrorsCount := 1
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Nil(t, err)
+		require.Equal(t, int32(tasksCount), runTasksCount)
+	})
+
+	t.Run("asterisks", func(t *testing.T) {
+		tasksCount := 5
+		workers := 3
+		maxErrors := 1
+
+		tasks := make([]Task, 0, tasksCount)
+
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				require.Eventually(t, func() bool { return true }, taskSleep*5, taskSleep)
+				return nil
+			})
+		}
+
+		start := time.Now()
+		err := Run(tasks, workers, maxErrors)
+		since := time.Since(start)
+
+		require.Nil(t, err)
+		require.LessOrEqualf(t, int64(since), int64(sumTime), "Tasks were not started in parallel")
+	})
+}
