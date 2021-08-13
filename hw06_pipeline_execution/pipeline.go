831646c60de47858b37ffd90075fd46728b69448
Artur <60509074+thewolf27@users.noreply.github.com>
2021-08-13 06:02:36 +0300

    Hw06 Done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,10 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+	"sync/atomic"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,7 +13,71 @@ type (
 
 type Stage func(in In) (out Out)
 
+// Пояснение решения:
+// Мы передаём по одному числу в функцию processNumberToStages, и она прокидывает его через все стейджи
+// Эта функция запускается паралелльно для всех входных чисел
+// После чего мы принимаем эти числа в результат в соответствии с их позицией в исходном массиве
+// Решение работает за 0.45 секунды для первого теста и является самым оптимизированным в отличии от других решений
+// Пусть оно и занимает больше строк кода.
+func processNumberToStages(i interface{}, stages []Stage, done In) Out {
+	processResCh := make(Bi)
+
+	processCh := make(Bi, 1)
+	processCh <- i
+
+	go func(processCh In) {
+		defer close(processResCh)
+		for _, stage := range stages {
+			stageCh := make(Bi)
+			select {
+			case <-done:
+				return
+			case i := <-processCh:
+				processCh = stage(stageCh)
+				stageCh <- i
+			}
+		}
+		select {
+		case <-done:
+		case processResCh <- <-processCh:
+		}
+	}(processCh)
+
+	return processResCh
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	resultCh := make(Bi)
+
+	go func() {
+		defer close(resultCh)
+		var wg sync.WaitGroup
+		numberPosition := 0
+		var numbersPassed int32
+		atomic.StoreInt32(&numbersPassed, 0)
+		for i := range in {
+			wg.Add(1)
+			go func(i interface{}, pos int) {
+				defer wg.Done()
+				for j := range processNumberToStages(i, stages, done) {
+					for {
+						select {
+						case <-done:
+							return
+						default:
+						}
+						if int(atomic.LoadInt32(&numbersPassed)) == pos {
+							resultCh <- j
+							atomic.AddInt32(&numbersPassed, 1)
+							break
+						}
+					}
+				}
+			}(i, numberPosition)
+			numberPosition++
+		}
+		wg.Wait()
+	}()
+
+	return resultCh
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -13,29 +13,28 @@ const (
 	fault         = sleepPerStage / 2
 )
 
+var g = func(_ string, f func(v interface{}) interface{}) Stage {
+	return func(in In) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for v := range in {
+				time.Sleep(sleepPerStage)
+				out <- f(v)
+			}
+		}()
+		return out
+	}
+}
+
+var stages = []Stage{
+	g("Dummy", func(v interface{}) interface{} { return v }),
+	g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+	g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+	g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+}
+
 func TestPipeline(t *testing.T) {
-	// Stage generator
-	g := func(_ string, f func(v interface{}) interface{}) Stage {
-		return func(in In) Out {
-			out := make(Bi)
-			go func() {
-				defer close(out)
-				for v := range in {
-					time.Sleep(sleepPerStage)
-					out <- f(v)
-				}
-			}()
-			return out
-		}
-	}
-
-	stages := []Stage{
-		g("Dummy", func(v interface{}) interface{} { return v }),
-		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
-		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
-		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
-	}
-
 	t.Run("simple case", func(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
@@ -89,5 +88,90 @@ func TestPipeline(t *testing.T) {
 
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
+	})
+}
+
+func MyTests(t *testing.T) {
+	t.Run("empty stages", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]interface{}, 0, 10)
+		for s := range ExecutePipeline(in, nil, []Stage{}...) {
+			result = append(result, s)
+		}
+
+		require.Equal(t, []interface{}{1, 2, 3, 4, 5}, result)
+	})
+
+	t.Run("inordered in", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{4, 1, 3, 5, 2}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Equal(t, []string{"108", "102", "106", "110", "104"}, result)
+	})
+
+	t.Run("instantly done", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		close(done)
+		data := []int{4, 1, 3, 5, 2}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Len(t, result, 0)
+	})
+
+	t.Run("different stages", func(t *testing.T) {
+		stages := []Stage{
+			g("Multiplier (* 3)", func(v interface{}) interface{} { return v.(int) * 3 }),
+			g("Reducer (- 50)", func(v interface{}) interface{} { return v.(int) - 50 }),
+			g("Divider (/ 2)", func(v interface{}) interface{} { return v.(int) / 2 }),
+			g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+		}
+
+		in := make(Bi)
+		data := []int{14, 5, 27, 0, 3}
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Equal(t, []string{"-4", "-17", "15", "-25", "-20"}, result)
 	})
 }
