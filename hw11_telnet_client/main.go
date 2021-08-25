02d5492ef7dc7da713c7af8fc2302fc5f6a8e60e
thewolf27 <wolf-front@yandex.ru>
2021-08-25 05:50:27 +0300

    wip

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"io/ioutil"
+	"net"
+)
+
 func main() {
+	host := net.JoinHostPort(address, "")
+
+	client := NewTelnetClient(host, timeout, ioutil.NopCloser(in), out)
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"io"
 	"time"
 )
@@ -12,7 +14,47 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelClient struct {
+	in  io.ReadCloser
+	out io.Writer
+}
+
+func (t TelClient) Connect() error {
+
+	return nil
+}
+
+func (t TelClient) Close() error {
+
+	return nil
+}
+
+func (t TelClient) Send() error {
+
+	return nil
+}
+
+func (t TelClient) Receive() error {
+
+	return nil
+}
+
+/*
+$ go-telnet --timeout=10s host port
+$ go-telnet mysite.ru 8080
+$ go-telnet --timeout=3s 1.1.1.1 123
+
+*/
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	t := TelClient{
+		in:  in,
+		out: out,
+	}
+
+	scanner := bufio.NewScanner(t)
+
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
+
 	// Place your code here.
 	return nil
 }
